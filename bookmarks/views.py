51ff30fe6643ef0346d717004621fd4472d785f3
jmestrovic <jakov.mestrovic@gmail.com>
2012-05-25 16:15:12 +0200

    Primjer do kraja 4. poglavlja.

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -2,17 +2,17 @@
 from django.http import HttpResponse, Http404
 from django.contrib.auth.models import User
 from django.template import Context
+from django.template import RequestContext
 from django.template.loader import get_template
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout
+from bookmarks.forms import *
+
 
 def main_page(request):
-    template = get_template('main_page.html')
-    variables = Context({
-        'head_title': u'Django Bookmarks',
-        'page_title': u'Welcome to Django Bookmarks',
-        'page_body': u'Where you can store and share bookmarks!'
-    })
-    output = template.render(variables)
-    return HttpResponse(output)
+    return render_to_response('main_page.html', RequestContext(request)
+)
 
 
 def user_page(request, username):
@@ -21,10 +21,36 @@ def user_page(request, username):
     except User.DoesNotExist:
         raise Http404(u'Requested user not found.')
     bookmarks = user.bookmark_set.all()
-    template = get_template('user_page.html')
-    variables = Context({
+    variables = RequestContext(request, {
         'username': username,
         'bookmarks': bookmarks
     })
-    output = template.render(variables)
-    return HttpResponse(output)
+    return render_to_response('user_page.html', variables)
+
+
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
+def register_page(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1'],
+                email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/register/success/')
+    else:
+        form = RegistrationForm()
+
+    variables = RequestContext(request, {
+        'form': form
+    })
+    return render_to_response(
+        'registration/register.html',
+        variables
+    )
--- django_bookmarks/urls.py
+++ django_bookmarks/urls.py
@@ -1,13 +1,29 @@
 from django.conf.urls import patterns, include, url
 from bookmarks.views import *
+import os
+from django.conf.urls.defaults import *
+from bookmarks.views import *
+from django.views.generic.simple import direct_to_template
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+site_media = os.path.join(
+    os.path.dirname(__file__), 'site_media'
+)
+
+
 urlpatterns = patterns('',
     (r'^$', main_page),
     (r'^user/(\w+)/$', user_page),
+    (r'^login/$', 'django.contrib.auth.views.login'),
+    (r'^logout/$', logout_page),
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': site_media}),
+    (r'^register/$', register_page),
+    (r'^register/success/$', direct_to_template, {'template': 'registration/register_success.html'}),
+
     # Examples:
     # url(r'^$', 'django_bookmarks.views.home', name='home'),
     # url(r'^django_bookmarks/', include('django_bookmarks.foo.urls')),
